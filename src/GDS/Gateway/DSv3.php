0d604856a163602e1064469a95cf999bc055cd50
Wade Khadder <wgk@google.com>
2015-07-01 09:44:30 -0700

    Creates no-op Gateway for DSv3
    Creates the class that will become the DSv3 Gateway. Currently it does nothing but extend necessary methods from Gateway.

---
+++ src/GDS/Gateway/DSv3.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace GDS\Gateway
+
+class DSv3 extends \GDS\Gateway {
+    protected function upsert(array $gds_entities) {
+    }
+    protected function extractAutoIDs() {
+    }
+    protected function fetchByKeyPart(array $arr_key_parts, $str_setter) {
+    }
+    public function deleteMulti(array $arr_entities){
+    }
+    public function gql($str_gql, $arr_params = NULL) {
+    }
+    public function getEndCursor() {
+    }
+    protected function createMapper() {
+    }
+    public function beginTransaction($bol_cross_group = FALSE) {
+    }
+}
