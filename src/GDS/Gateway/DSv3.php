ab515a07adead664f61565e678ff89f792748fc4
Wade Khadder <wgk@google.com>
2015-07-01 10:00:24 -0700

    Adds initial upsert in DSv3 Gateway
    Depends on a Mapper that has not been implemented yet

--- src/GDS/Gateway/DSv3.php
+++ src/GDS/Gateway/DSv3.php
@@ -4,7 +4,19 @@ namespace GDS\Gateway
 
 class DSv3 extends \GDS\Gateway {
     protected function upsert(array $gds_entities) {
+        $request = new PutRequest();
+        $autoid_entities = [];
+        foreach($gds_entities as $gds_entity) {
+            $g_entity = $request->addEntity();
+            if($this->needsAutoId($gds_entity)) {
+                $autoid_entities[] = $gds_entity;
+            }
+            $this->determineMapper($gds_entity)
+                 ->mapToGoogle($gds_entity, $g_entity);
+        }
+        $this->execute('Commit', $request, new PutResponse());
     }
+
     protected function extractAutoIDs() {
     }
     protected function fetchByKeyPart(array $arr_key_parts, $str_setter) {
@@ -18,5 +30,13 @@ class DSv3 extends \GDS\Gateway {
     protected function createMapper() {
     }
     public function beginTransaction($bol_cross_group = FALSE) {
+    }
+
+    private function needsAutoId(Entity $gds_entity) {
+            return $gds_entity->getKeyId() == NULL && $gds_entity->getKeyName() == NULL;
+    }
+
+    private function execute($command, $request, $response) {
+        ApiProxy::makeSyncCall('datastore_v3', $command, $request, $response, 60);
     }
 }
